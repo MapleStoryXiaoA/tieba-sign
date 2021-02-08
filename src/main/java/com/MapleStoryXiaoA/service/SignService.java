f1166cca46da0f81bacc38b528a6066804d78e97
maomaozizi <2655411621@qq.com>
2021-02-08 22:20:11 +0800

    修改

--- src/main/java/com/MapleStoryXiaoA/service/SignService.java
+++ src/main/java/com/MapleStoryXiaoA/service/SignService.java
@@ -1,143 +1,54 @@
 package com.MapleStoryXiaoA.service;
 
+import com.MapleStoryXiaoA.entity.Cookie;
 import com.MapleStoryXiaoA.util.Encryption;
 import com.MapleStoryXiaoA.util.Request;
-import io.reactivex.Single;
-import io.vertx.core.json.JsonArray;
-import io.vertx.core.json.JsonObject;
-import io.vertx.reactivex.core.buffer.Buffer;
-import io.vertx.reactivex.ext.web.client.HttpResponse;
-import lombok.extern.slf4j.Slf4j;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import io.vertx.core.json.JsonObject;
+import io.vertx.reactivex.core.MultiMap;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class SignService {
-    //获取用户所有关注贴吧
-    private final String LIKE_URL = "https://tieba.baidu.com/mo/q/newmoindex";
-    //获取用户的tbs
-    private final String TBS_URL = "http://tieba.baidu.com/dc/common/tbs";
-    //贴吧签到接口
-    private final String SIGN_URL = "http://c.tieba.baidu.com/c/c/forum/sign";
-    //存储用户所关注的贴吧
-    private List<String> followList = new ArrayList<String>();
-    //签到成功的贴吧列表
-    private List<String> successList = new ArrayList<String>();
-    //签到失败的贴吧列表
-    private List<String> failList = new ArrayList<String>();
-    //还未签到的贴吧列表()
-    private ConcurrentHashMap<String, Integer> todoList = new ConcurrentHashMap<>();
-    //用户的tbs
-    private String tbs;
+	//获取用户所有关注贴吧
+	private final String LIKE_URL = "https://tieba.baidu.com/mo/q/newmoindex";
+	//贴吧签到接口
+	private final String SIGN_URL = "http://c.tieba.baidu.com/c/c/forum/sign";
+	private final Request request;
 
-    public void start() {
-        getTbs();
-        getFollow();
-        runSign();
-    }
+	public SignService(Cookie cookie) {
+		this.request = new Request(cookie);
+	}
 
+	public void start() {
+		//获取用户的tbs
+		String tBSURL = "http://tieba.baidu.com/dc/common/tbs";
+		request.getSync(tBSURL).map(response -> response.bodyAsJsonObject().getString("tbs"))
+				.map(tbs -> request.getSync(LIKE_URL)
+						.map(response -> response.bodyAsJsonObject().getJsonObject("data").getJsonArray("like_forum"))
+						.flattenAsFlowable(x -> x)
+						.filter(obj -> 0 == (((JsonObject) obj).getInteger("is_sign")))
+						.map(obj -> ((JsonObject) obj).getString("forum_name"))
+						.map(kw -> {
+							final MultiMap params = MultiMap.caseInsensitiveMultiMap();
+							params.set("kw", kw);
+							params.set("sign", Encryption.enCodeMd5("kw=" + kw + "tbs=" + tbs + "tiebaclient!!!"));
+							return request.postAsync(SIGN_URL, params)
+									.map(resp -> {
+										JsonObject jsonObject = resp.bodyAsJsonObject();
+										String errorCode = jsonObject.getString("error_code");
+										if ("0".equals(errorCode)) {
+											Integer signBonusPoint = jsonObject.getJsonObject("user_info").getInteger("sign_bonus_point");
+											Integer userSignRank = jsonObject.getJsonObject("user_info").getInteger("user_sign_rank");
+											log.info("{}吧签到完成，经验值加{}，你是今天第{}个签到的", kw, signBonusPoint, userSignRank);
+											return true;
+										} else {
+											log.warn("{}吧签到失败,失败原因为【{}】", kw, jsonObject.getString("error_msg"));
+											return false;
+										}
+									});
+						}))
+				.subscribe();
 
-    /**
-     * 进行登录，获得tbs ，签到的时候需要用到这个参数
-     */
-    public void getTbs() {
-        try {
-            JsonObject jsonObject = Request.getSync(TBS_URL);
-            tbs = jsonObject.getString("tbs");
-            log.info("获取tbs为{}", tbs);
-        } catch (Exception e) {
-            log.error("获取tbs出错", e);
-        }
-    }
-
-    /**
-     * 获取用户所关注的贴吧列表
-     */
-    public void getFollow() {
-        try {
-            JsonObject jsonObject = Request.getSync(LIKE_URL);
-            JsonArray jsonArray = jsonObject.getJsonObject("data").getJsonArray("like_forum");
-            // 获取用户所有关注的贴吧
-            for (Object array : jsonArray) {
-                followList.add(((JsonObject) array).getString("forum_name"));
-                if (0 == (((JsonObject) array).getInteger("is_sign"))) {
-                    // 将未签到的贴吧加入到 follow 中，待签到
-                    todoList.put(((JsonObject) array).getString("forum_name"), 0);
-                } else {
-                    // 将已经成功签到的贴吧，加入到 success
-                    successList.add(((JsonObject) array).getString("forum_name"));
-                }
-            }
-            log.info("总共关注了{}个贴吧", followList.size());
-            log.info("未签到的贴吧:{}", todoList.keySet());
-            log.info("已签到的贴吧:{}", successList);
-        } catch (Exception e) {
-            log.error("获取关注的贴吧列表错误", e);
-        }
-    }
-
-
-    /**
-     * 开始签到
-     */
-    public void runSign() {
-        Map<String, String> params = new HashMap<>();
-        params.put("tbs", tbs);
-        // 当执行 5 轮所有贴吧还未签到成功就结束操作
-        final int flag = 5;
-        try {
-            for (; ; ) {
-                if (todoList.size() <= 0) {
-                    return;
-                }
-                Iterator<Map.Entry<String, Integer>> iterator = todoList.entrySet().iterator();
-                while (iterator.hasNext()) {
-                    Map.Entry<String, Integer> entry = iterator.next();
-                    String kw = entry.getKey();
-                    params.put("kw", kw);
-                    params.put("sign", Encryption.enCodeMd5("kw=" + kw + "tbs=" + tbs + "tiebaclient!!!"));
-                    log.info("{}吧签到中", kw);
-                    Single<HttpResponse<Buffer>> httpResponseSingle = Request.postAsync(SIGN_URL, params);
-                    httpResponseSingle.subscribe(
-                            resp -> {
-                                JsonObject jsonObject = resp.bodyAsJsonObject();
-                                String error_code = jsonObject.getString("error_code");
-                                if ("0".equals(error_code)) {
-                                    Integer sign_bonus_point = jsonObject.getJsonObject("user_info").getInteger("sign_bonus_point");
-                                    Integer user_sign_rank = jsonObject.getJsonObject("user_info").getInteger("user_sign_rank");
-                                    log.info("{}吧签到完成，经验值加{}，你是今天第{}个签到的", kw, sign_bonus_point, user_sign_rank);
-                                    successList.add(kw);
-                                    todoList.remove(kw);
-                                } else {
-                                    log.warn("{}吧签到失败,失败原因为【{}】", kw, jsonObject.getString("error_msg"));
-                                    if (todoList.containsKey(kw)) {
-                                        int doneTimes = todoList.get(kw) + 1;
-                                        if (doneTimes >= flag) {
-                                            log.warn("{}吧签到失败{}次,不在继续签到", kw, flag);
-                                            todoList.remove(kw);
-                                            failList.add(kw);
-                                        } else {
-                                            log.warn("{}吧将进入重新尝试签到列表...", kw);
-                                            todoList.put(kw, doneTimes);
-                                        }
-                                    }
-                                }
-                            }
-                    );
-                }
-
-                Thread.sleep(1000);//签太快百度会报失败
-            }
-        } catch (Exception e) {
-            log.error("签到出错", e);
-        } finally {
-            log.info("今日签到成功的贴吧有{}", successList);
-            log.info("今日签到失败的贴吧有{}", failList);
-        }
-    }
+	}
 }
--- src/main/java/com/MapleStoryXiaoA/util/Request.java
+++ src/main/java/com/MapleStoryXiaoA/util/Request.java
@@ -1,8 +1,8 @@
 package com.MapleStoryXiaoA.util;
 
 import com.MapleStoryXiaoA.entity.Cookie;
+
 import io.reactivex.Single;
-import io.vertx.core.json.JsonObject;
 import io.vertx.reactivex.core.MultiMap;
 import io.vertx.reactivex.core.Vertx;
 import io.vertx.reactivex.core.buffer.Buffer;
@@ -11,62 +11,41 @@ import io.vertx.reactivex.ext.web.client.HttpResponse;
 import io.vertx.reactivex.ext.web.client.WebClient;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Map;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-
+/**
+ * @author xiangzicheng-PC
+ */
 @Slf4j
 public class Request {
 
-    private static final Cookie cookie = Cookie.getInstance();
+	private final Cookie cookie;
+	private static final Vertx VERTX;
+	private static final WebClient CLIENT;
+	private static final MultiMap HEADERS = MultiMap.caseInsensitiveMultiMap();
 
-    public static JsonObject getSync(String url) throws ExecutionException, InterruptedException {
-        Vertx vertx = Vertx.vertx();
-        WebClient client = WebClient.create(vertx);
-//        log.info("webClient正在请求{}", url);
+	static {
+		VERTX = Vertx.vertx();
+		CLIENT = WebClient.create(VERTX);
+		HEADERS.add("connection", "keep-alive");
+		HEADERS.add("Content-Type", "application/x-www-form-urlencoded");
+		HEADERS.add("charset", "UTF-8");
+		HEADERS.add("User-Agent", "bdtb for Android 8.4.0.1");
+		HEADERS.add("net", "3");
+	}
 
-        final CompletableFuture<JsonObject> jsonObject = new CompletableFuture<>();
+	public Request(Cookie cookie) {
+		this.cookie = cookie;
+	}
 
-        HttpRequest<Buffer> request = client.getAbs(url);
-        MultiMap headers = request.headers();
-        headers.clear();
-        headers.add("connection", "keep-alive");
-        headers.add("Content-Type", "application/x-www-form-urlencoded");
-        headers.add("charset", "UTF-8");
-        headers.add("User-Agent", "bdtb for Android 8.4.0.1");
-        headers.add("net", "3");
-        headers.add("Cookie", cookie.getCookie());
-//        headers.add("Accept-Encoding", "gzip");
+	public Single<HttpResponse<Buffer>> getSync(String url) {
+		return CLIENT.postAbs(url).putHeaders(HEADERS).putHeader("Cookie", cookie.getCookie())
+				.rxSend();
+	}
 
-        request.send(ar -> {
-            if (ar.succeeded()) {
-                // 获取响应
-                HttpResponse<Buffer> response = ar.result();
-//                log.info("webClient收到{}响应状态码为{},响应体为{}", url, response.statusCode(), response.bodyAsString("utf-8"));
-                jsonObject.complete(response.bodyAsJsonObject());
-            } else {
-                log.error("webClient get请求出错{}", ar.cause().getMessage());
-            }
-        });
-        return jsonObject.get();
-    }
-
-    public static Single<HttpResponse<Buffer>> postAsync(String url, Map<String, String> params) {
-        Vertx vertx = Vertx.vertx();
-        WebClient client = WebClient.create(vertx);
-//        log.info("webClient正在请求{}", url);
-
-        HttpRequest<Buffer> bufferHttpRequest = client.postAbs(url);
-        MultiMap headers = bufferHttpRequest.headers();
-        headers.clear();
-        headers.add("connection", "keep-alive");
-        headers.add("Content-Type", "application/x-www-form-urlencoded");
-        headers.add("charset", "UTF-8");
-        headers.add("User-Agent", "bdtb for Android 8.4.0.1");
-        headers.add("net", "3");
-        headers.add("Cookie", cookie.getCookie());
-        bufferHttpRequest.queryParams().addAll(params);
-        return bufferHttpRequest.rxSend();
-    }
+	public Single<HttpResponse<Buffer>> postAsync(String url, MultiMap params) {
+		final HttpRequest<Buffer> bufferHttpRequest = CLIENT.postAbs(url).putHeaders(HEADERS);
+		params.forEach(entry -> bufferHttpRequest.addQueryParam(entry.getKey(), entry.getValue()));
+		return bufferHttpRequest.putHeader("Cookie", cookie.getCookie())
+				.rxSend();
+	}
 
 }
